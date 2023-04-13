60fbaf4a7d2a3ce1d688dbac954fa5520cc87e48
logonrmlocal <felipe.mgomes94@gmail.com>
2023-04-13 18:25:00 -0300

    primeiro commit java logica

---
+++ JAVA Tokio 2023/src/logica/aulas/SegundoPrograma.java
@@ -0,0 +1,28 @@
+package logica.aulas;
+
+import java.util.Scanner;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada =new Scanner(System.in);
+		String nome = entrada.nextLine();
+		
+		System.out.println("Bem-vindo, " + nome);
+		System.out.println("Digite sua idade");
+		System.out.println("Sua idade é: " + entrada.nextInt());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
